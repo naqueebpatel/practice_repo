43f15b73b595a6d13f7b6eaab0c39a23998088da
FaizanKhan78 <faizankhan9309@gmail.com>
2023-11-07 17:19:51 +0530

    faizan

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-// praticing repo
-
-const number = 2;
-// Faizan Khan 786.
-
-if (number % 2 == 0) {
-    console.log("it is an even number");
-}
-else {
-    console.log("it is NOT AN EVEN NUMBER");
-
-
-    //faisalcode added
-
-    //naqueeb is editing
-
-    //react code here from faizan about navbar is here
-
+function bubbleSort(array) {
+    for (const a of array) {
+        console.table(a);
+    }
 }
 
+let array = [ 1, 32, 23, 43, 221, 21 ];
+bubbleSort(array);
