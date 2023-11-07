41dd7d3392944d93cbbc35e17b8ab79d165356af
FaizanKhan78 <faizankhan9309@gmail.com>
2023-11-07 17:23:57 +0530

    Hello World

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-// praticing repo
-
-const number = 2;
-// Faizan Khan 786.
-
-if (number % 2 == 0) {
-    console.log("it is an even number");
-}
-else {
-    console.log("it is NOT AN EVEN NUMBER");
-
-    //naqueeb is editing
-
-    //react code here from faizan about navbar is here
-
-}
+console.log("Hello World \n");
