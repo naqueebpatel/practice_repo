1f65694525deb62b73c22066ab94a388c1ea721e
FaizanKhan78 <faizankhan9309@gmail.com>
2023-11-07 17:31:30 +0530

    Hello World

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-function bubbleSort(array) {
-    for (const a of array) {
-        console.table(a);
-    }
-}
-
-let array = [ 1, 32, 23, 43, 221, 21 ];
-bubbleSort(array);
+console.log("Hello World");
